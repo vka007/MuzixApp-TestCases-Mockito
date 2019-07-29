399d7994038ccdc241bda56ff1ca1dfc3b144300
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-07-29 17:37:58 +0530

    Removed setters  used intead @setter annotation

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -18,22 +18,5 @@ public class Track {
     String name;
     String comment;
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-/* public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }*/
+   
 }
